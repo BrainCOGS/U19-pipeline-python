6afe47f95ffd2c1a2116ede112073c6a6bfec439
shenshan <shenshanpku@gmail.com>
2020-11-10 11:28:21 -0800

    Put the store config into __init__.py

--- u19_pipeline/__init__.py
+++ u19_pipeline/__init__.py
@@ -1,2 +1,8 @@
 import datajoint as dj
 dj.config['database.prefix'] = 'u19_'
+
+dj.config['stores']['meso'] = {
+    'location': '/Volumes/u19_dj/external_dj_blobs/meso',
+    'protocol': 'file',
+    'subfolding': [2, 2]
+}
