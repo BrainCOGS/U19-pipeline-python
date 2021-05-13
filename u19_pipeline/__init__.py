31c1efd93dcfbe5f5ffb7b7d3ff2fc24fe1191d1
shenshan <shenshanpku@gmail.com>
2021-05-13 15:33:34 -0400

    update notebooks of imaging element

--- u19_pipeline/__init__.py
+++ u19_pipeline/__init__.py
@@ -1,22 +1,8 @@
 from os.path import join, dirname
 import os
-from dotenv import load_dotenv
-load_dotenv(join(dirname(__file__), '..', '.env'))
-
 import datajoint as dj
 
-dj.config['database.prefix'] = 'u19_'
-
-dj.config['database.host'] = os.getenv('DJ_HOST')
-dj.config['database.user'] = os.getenv('DJ_USER')
-dj.config['database.password'] = os.getenv('DJ_PASS')
-
 dj.config['enable_python_native_blobs'] = True
-
-dj.config['custom'] = dict(
-    ephys_root_data_dir='/mnt/bucket/labs/tank/schottdorf/NPX/',
-    imaging_root_data_dir='/mnt/bucket/braininit/RigData/mesoscope/imaging/imaging_pipe1/'
-)
 
 dj.config['stores'] = {
     'meso':
