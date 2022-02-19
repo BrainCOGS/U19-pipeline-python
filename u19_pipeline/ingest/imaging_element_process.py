4af1a89ebc829baed2b4b6270f40b613d9a62a5a
Kabilar Gunalan <kg7524@spock-login.pni.Princeton.EDU>
2022-02-18 21:35:07 -0500

    Rename method

--- u19_pipeline/ingest/imaging_element_process.py
+++ u19_pipeline/ingest/imaging_element_process.py
@@ -1,33 +1,25 @@
 from u19_pipeline.imaging_element import scan_element, imaging_element
 
 
-def populate(display_progress=True):
+def run(display_progress=True):
 
     populate_settings = {'display_progress': display_progress, 'reserve_jobs': False, 'suppress_errors': False}
 
-    print('\n---- Populate scan.ScanInfo ----')
+    print('\n---- Populate imported and computed tables ----')
     scan_element.ScanInfo.populate(**populate_settings)
 
-    print('\n---- Populate imaging.Processing ----')
     imaging_element.Processing.populate(**populate_settings)
 
-    print('\n---- Populate imaging.MotionCorrection ----')
     imaging_element.MotionCorrection.populate(**populate_settings)
 
-    print('\n---- Populate imaging.Segmentation ----')
     imaging_element.Segmentation.populate(**populate_settings)
 
-    print('\n---- Populate imaging.MaskClassification ----')
-    imaging_element.MaskClassification.populate(**populate_settings)
-
-    print('\n---- Populate imaging.Fluorescence ----')
     imaging_element.Fluorescence.populate(**populate_settings)
 
-    print('\n---- Populate imaging.Activity ----')
     imaging_element.Activity.populate(**populate_settings)
 
-    print('\n---- Successfully completed workflow_imaging/populate.py ----')
+    print('\n---- Successfully completed workflow_imaging/process.py ----')
 
 
 if __name__ == '__main__':
-    populate()
+    run()
