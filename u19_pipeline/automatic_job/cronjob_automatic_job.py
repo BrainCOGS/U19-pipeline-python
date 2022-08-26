1db460eb1c17ebdad126c37d73dda839588f6ad8
Alvaro Luna <alvalunasan@gmail.com>
2022-08-26 16:04:39 -0400

    time sleep 5 not 50

--- u19_pipeline/automatic_job/cronjob_automatic_job.py
+++ u19_pipeline/automatic_job/cronjob_automatic_job.py
@@ -10,12 +10,12 @@ import u19_pipeline.automatic_job.recording_process_handler as rec_process_handl
 
 # Check recordings and then jobs
 rec_handler.RecordingHandler.pipeline_handler_main()
-time.sleep(50)
+time.sleep(5)
 rec_process_handler.RecProcessHandler.pipeline_handler_main()
-time.sleep(50)
+time.sleep(5)
 rec_process_handler.RecProcessHandler.pipeline_handler_main()
 
-time.sleep(50)
+time.sleep(5)
 #Check if we need to delete empty directories
 rec_process_handler.RecProcessHandler.check_job_process_deletion()
 
