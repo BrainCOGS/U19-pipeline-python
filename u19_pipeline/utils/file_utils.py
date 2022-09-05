4b1d6ceb6a06432365c70fff0b5b198e8e9d9ec6
Alvaro Luna <alvalunasan@gmail.com>
2022-09-05 11:17:27 -0400

    write files with permissions

--- u19_pipeline/utils/file_utils.py
+++ u19_pipeline/utils/file_utils.py
@@ -1,7 +1,17 @@
-
+import os
 
 def write_file(path, text):
 
-    f = open(path, "w")
-    f.write(text)
-    f.close()
+    os.umask(0)
+    descriptor = os.open(
+    path=path,
+    flags=(
+        os.O_WRONLY  # access mode: write only
+        | os.O_CREAT  # create if not exists
+        | os.O_TRUNC  # truncate the file to zero
+    ),
+    mode=0o664
+    )
+
+    with open(descriptor, 'w') as fh:
+        fh.write(text)
