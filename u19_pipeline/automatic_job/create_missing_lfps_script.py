9c7249b201ed18d550b5eff90bd437528ffef523
Alvaro Luna <alvalunasan@gmail.com>
2022-10-22 09:37:52 -0400

    create lfp script typo

--- u19_pipeline/automatic_job/create_missing_lfps_script.py
+++ u19_pipeline/automatic_job/create_missing_lfps_script.py
@@ -14,7 +14,7 @@ def main(recording_id):
 
     recording_processes = (recording_process.Processing() & rec_query).fetch('job_id', 'recording_id', 'fragment_number', 'recording_process_pre_path', as_dict=True)
     #Create lfp trace if needed (neuropixel 2.0 probes)
-    recording_directory = (recording.Recording & rec_query['query_key']).fetch1('recording_directory')
+    recording_directory = (recording.Recording & rec_query).fetch1('recording_directory')
     recording_directory = pathlib.Path(dj.config['custom']['ephys_root_data_dir'][0], recording_directory).parent.as_posix()
     for i in recording_processes:
         probe_dir = pathlib.Path(dj.config['custom']['ephys_root_data_dir'][0], i['recording_process_pre_path']).as_posix()
