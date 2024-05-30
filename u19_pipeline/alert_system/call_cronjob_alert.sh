9618cfdc75fbcc9069a72583a52aa36dc9dd2eb4
Alvaro Luna <alvalunasan@gmail.com>
2024-05-30 17:06:21 -0600

    new python conda env for u19proc

--- u19_pipeline/alert_system/call_cronjob_alert.sh
+++ u19_pipeline/alert_system/call_cronjob_alert.sh
@@ -6,7 +6,7 @@ echo $(pwd)
 source /home/u19prod@pu.win.princeton.edu/.bashrc
 source /home/u19prod@pu.win.princeton.edu/.bash_profile
 
-conda activate U19-pipeline_python_env
+conda activate U19-pipeline_python_env2
 cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/U19-pipeline_python/"
 git pull
 python ./u19_pipeline/alert_system/cronjob_alert.py
