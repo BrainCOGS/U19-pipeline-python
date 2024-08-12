69cba54105286292f7c933b972bc962c6278634a
Alvaro Luna <alvalunasan@gmail.com>
2024-08-12 12:44:08 -0600

    debug error retrieval automation pipeline 2

--- u19_pipeline/automatic_job/clusters_paths_and_transfers.py
+++ u19_pipeline/automatic_job/clusters_paths_and_transfers.py
@@ -277,7 +277,10 @@ def get_error_log_str(recording_process_id):
         error_log_data = error_log_data.replace("activate the default environment with 'conda activate' or create a new environment to customize with 'conda create'.\n",'')
 
     print('error_log_data   xxxxxxxxxx')
-    print(error_log_data)    
+    if len(error_log_data) > 400:
+        print(error_log_data[-400:])
+    else:
+        print(error_log_data)
     print('error_log_data   xxxxxxxxxx')
 
     return error_log_data
