92fa17555a0f3dfe3c36c368ea394a26317af394
Christian Tabedzki <35670232+tabedzki@users.noreply.github.com>
2024-09-25 12:48:40 -0400

    Updating cronjobs

--- u19_pipeline/automatic_job/call_cronjob_automatic_job.sh
+++ u19_pipeline/automatic_job/call_cronjob_automatic_job.sh
@@ -7,7 +7,7 @@ source /home/u19prod@pu.win.princeton.edu/.bashrc
 source /home/u19prod@pu.win.princeton.edu/.bash_profile
 
 
-conda activate U19-pipeline_python_env2
+conda activate U19-pipeline_python_env3
 cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/U19-pipeline_python/"
 git pull
 python ./u19_pipeline/automatic_job/cronjob_automatic_job.py
--- u19_pipeline/automatic_job/call_pupillometry_queue_jobs.sh
+++ u19_pipeline/automatic_job/call_pupillometry_queue_jobs.sh
@@ -7,7 +7,7 @@ echo $(pwd)
 source /home/u19prod@pu.win.princeton.edu/.bashrc
 source /home/u19prod@pu.win.princeton.edu/.bash_profile
 
-conda activate U19-pipeline_python_env2
+conda activate U19-pipeline_python_env3
 cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/U19-pipeline_python/"
 git pull
 python ./u19_pipeline/automatic_job/pupillometry_handler_script.py
