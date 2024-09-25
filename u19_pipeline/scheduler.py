92dbe74ace684b3254930ff6b21f2aef1bed4885
Alvaro Luna <alvalunasan@gmail.com>
2024-09-25 12:13:10 -0600

    inputOutput profile tables added

--- u19_pipeline/scheduler.py
+++ u19_pipeline/scheduler.py
@@ -33,6 +33,18 @@ class RecordingProfile(dj.Manual):
     """
 
 @schema
+class InputOutputProfile(dj.Manual):
+    definition = """
+    # Input/Outuput profile registry table
+    input_output_profile_id          : int AUTO_INCREMENT           # numeric_id for Input/Output profile
+    ---
+    ->lab.User
+    input_output_profile_name         : varchar(32)                 # Input/Output profile name
+    input_output_profile_description  : varchar(255)                # Input/Output profile description
+    input_output_profile_date         : date                        # Input/Output profile creation date
+    """
+
+@schema
 class Schedule(dj.Manual):
     definition = """
     date                         : date                  # Full date
@@ -45,4 +57,39 @@ class Schedule(dj.Manual):
     -> [nullable] InputOutputProfile                      # Reference to `InputOutputProfile`
     """
 
+@schema
+class InputOutputRig(dj.Lookup):
+    definition = """
+    # Which Inputs and Outputs can be installed in rigs (for RigTester purposes)
+    input_output_name          : varchar(32)                 # Name and ID for Input/Output 
+    ---
+    description                : varchar(255)                # Input/Output description
+    direction                  : enum('Input', 'Output')     # Input/Output direction (for RigTester purposes)
+    test_type                  : enum('Automatic', 'Manual') # Manual if technician have to check test (e.g. Speaker) Automatic otherwise
+    """
+    contents = [
+            ['Arduino', '', 'Input', 'Automatic'],
+            ['MotionSensor',  '', 'Input', 'Automatic'],
+            ['LateralCamera',  '', 'Input', 'Automatic'],
+            ['TopCamera',  '', 'Input', 'Automatic'],
+            ['Speakers',  '', 'Output', 'Manual'],
+            ['Motors',  '', 'Output', 'Manual'],
+            ['Reward',  '', 'Output', 'Manual'],
+            ['Laser',  '', 'Output', 'Manual'],
+            ['LeftPuff',  '', 'Output', 'Manual'],
+            ['RightPuff',  '', 'Output', 'Manual'],
+            ['LeftReward',  '', 'Output', 'Manual'],
+            ['RightReward',  '', 'Output', 'Manual'],
+            ['Lickometer',  '', 'Output', 'Manual'],
+    ]
 
+@schema
+class InputOutputProfileList(dj.Manual):
+    definition = """
+    # InputOutputProfile full list of InputsOutputs and type of test for each 
+    ->lab.InputOutputProfile
+    input_output_num           : int                         # # Of Input/Output for this profile        
+    ---
+    ->lab.InputOutputRig
+    check_type                 : enum('Mandatory','Optional') # Prevent training if missing this input/output
+    """
