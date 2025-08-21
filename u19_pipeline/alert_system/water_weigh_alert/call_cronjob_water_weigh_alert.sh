b9134989777e7ecd7a6c9a3515fde560fd22c89e
mmagos28 <119622721+mmagos28@users.noreply.github.com>
2025-08-21 11:17:11 -0400

    rename water weigh cronjob2

--- u19_pipeline/alert_system/water_weigh_alert/call_cronjob_water_weigh_alert.sh
+++ u19_pipeline/alert_system/water_weigh_alert/call_cronjob_water_weigh_alert.sh
@@ -8,4 +8,4 @@ source /home/u19prod@pu.win.princeton.edu/.bash_profile
 
 conda activate U19-pipeline_python_env3
 cd "/home/u19prod@pu.win.princeton.edu/Datajoint_projs/U19-pipeline_python/"
-python ./u19_pipeline/alert_system/live_monitor_alert/cronjob_water_weigh_alert.py
+python ./u19_pipeline/alert_system/water_weigh_alert/cronjob_water_weigh_alert.py
