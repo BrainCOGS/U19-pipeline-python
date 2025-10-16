91d3b60b0f2270f5c9bff6eecb2a0f665ac1d879
Alvaro Luna <alvalunasan@gmail.com>
2025-10-16 12:36:22 -0600

    merge alerts in tech_alert shell

--- u19_pipeline/alert_system/locked_tables_alert/cronjob_locked_tables_alert.py
+++ u19_pipeline/alert_system/locked_tables_alert/cronjob_locked_tables_alert.py
@@ -6,7 +6,6 @@ try_find_conf_file()
 time.sleep(0.1)
 
 import u19_pipeline.alert_system.locked_tables_alert.locked_tables_alert as lta
-import u19_pipeline.alert_system.schedule_check_alert.schedule_check_alert as sca
 
 lta.main_locked_tables_alert()
-sca.main_schedule_check_alert()
+
