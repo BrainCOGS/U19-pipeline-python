36277dad39b5797741a2a0a6a9312483bb5501ee
Alvaro Luna <alvalunasan@gmail.com>
2025-11-17 14:04:44 -0600

    live monitor alert internal

--- u19_pipeline/alert_system/live_monitor_alert/live_monitor_alert.py
+++ u19_pipeline/alert_system/live_monitor_alert/live_monitor_alert.py
@@ -187,7 +187,13 @@ def main_live_monitor_alert():
             live_stats['alert_nvio'] = live_stats['seconds_elapsed_last_stat_nvio'] > SECONDS_ALERT
 
             live_stats['seconds_elapsed_session_started'] = (right_now_est- live_stats['session_start_time']).dt.total_seconds()
-            live_stats['alert_vio'] = live_stats['seconds_elapsed_session_started'] > SECONDS_ALERT & pd.isna(live_stats['last_non_violation_trial']) & ~pd.isna(live_stats['last_violation_trial'])
+            live_stats['alert_vio'] = live_stats['seconds_elapsed_session_started'] > SECONDS_ALERT & (pd.isna(live_stats['last_non_violation_trial'])) & (~pd.isna(live_stats['last_violation_trial']))
+
+            print("pd.isna(live_stats['last_non_violation_trial']")
+            print(pd.isna(live_stats['last_non_violation_trial']))
+
+            print("(~pd.isna(live_stats['last_violation_trial']))")
+            print((~pd.isna(live_stats['last_violation_trial'])))
 
             print(live_stats.T)
 
