cb637387d30be5900e963bb1bd12e5597cbf235d
Alvaro Luna <alvalunasan@gmail.com>
2025-11-18 19:37:05 -0600

    live session stats deletion and log deletion in 3am main alert

--- u19_pipeline/alert_system/cronjob_alert.py
+++ u19_pipeline/alert_system/cronjob_alert.py
@@ -7,6 +7,11 @@ time.sleep(1)
 
 import datajoint as dj
 import u19_pipeline.alert_system.main_alert_system as mas
+import u19_pipeline.alert_system.log_deletion.old_log_deletion as old
+import u19_pipeline.alert_system.live_session_stats_deletion.live_session_stats_deletion as lssd
+
 
 mas.main_alert_system()
+old.main_old_log_deletion()
+lssd.main_live_session_stats_deletion()
 
